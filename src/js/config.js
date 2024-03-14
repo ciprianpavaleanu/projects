fb6fb451617d4d6d09fde5e91a3a3db357efb5dc
Păvăleanu Ciprian <155653543+ciprianpavaleanu@users.noreply.github.com>
2024-03-14 19:50:51 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '21b6292f-3e23-4509-9b26-3e8305374918';
+export const MODAL_CLOSE_SEC = 2.5;
